7da9deffdc547ada862223e2966ef28a7cee58e3
marslinoED <lino.edward11@gmail.com>
2026-03-08 01:46:27 +0200

    Commit : 448

--- call_api/index.js
+++ call_api/index.js
@@ -54,8 +54,8 @@ app.use(express.json());
 // Development logging
 app.use(morgan("dev"));
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files from the public directory (but not index.html at root)
+app.use(express.static(path.join(__dirname, "public"), { index: false }));
 
 // Routes
 app.use("/api/v1/call", callRoutes);
